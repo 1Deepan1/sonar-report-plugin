54a8eaca172a8bdd34ce9de2ee411890978e5637
deepan <umail2deep@gmail.com>
2015-06-25 18:27:23 +0530

    added time machine

---
+++ src/main/java/org/ericsson/sonar/plugin/builder/SonarTimeMachine.java
@@ -0,0 +1,14 @@
+package org.ericsson.sonar.plugin.builder;
+
+import org.sonar.api.config.Settings;
+
+public class SonarTimeMachine extends SonarClient{
+
+	public SonarTimeMachine(Settings settings) {
+		super(settings);
+		
+	}
+	
+	
+
+}
