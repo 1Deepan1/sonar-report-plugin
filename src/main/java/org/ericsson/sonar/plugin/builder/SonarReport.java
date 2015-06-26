aae790551959f5c7be75ac2c0e2c9ff0132d7293
Maheshram <maheshrampixel@gmail.com>
2015-06-26 11:49:06 +0530

    Html generation

--- src/main/java/org/ericsson/sonar/plugin/builder/SonarReport.java
+++ src/main/java/org/ericsson/sonar/plugin/builder/SonarReport.java
@@ -1,5 +1,21 @@
 package org.ericsson.sonar.plugin.builder;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.OutputStream;
+import java.io.StringReader;
+
+import javax.xml.transform.Source;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerConfigurationException;
+import javax.xml.transform.TransformerException;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.TransformerFactoryConfigurationError;
+import javax.xml.transform.stream.StreamResult;
+import javax.xml.transform.stream.StreamSource;
+
+import org.ericsson.sonar.exception.SonarReporException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.sonar.api.config.Settings;
@@ -20,14 +36,31 @@ public class SonarReport {
 		SonarMetrics sonarMetrics = new SonarMetrics(settings);
 		String[] metrics = sonarMetrics.getMetricsName();
 		SonarResources sonarResources = new SonarResources(metrics,settings);
-		System.out.println("<final>" + sonarResources.getAllResources()
-				+ "</final>");
+		String projectHome = settings.getProperties().get("sonar.projectHome");		
+		String sonarPath = projectHome+File.separator+".sonar";
+		File sonarDir = new File(sonarPath);
+		if(!sonarDir.exists()){
+			sonarDir.mkdir();
+		}
+		StreamSource source = new StreamSource(new StringReader("<final>" + sonarResources.getAllResources()+ "</final>"));
+		generateHtml(source, sonarPath);
 	}
+	
+	public void generateHtml(StreamSource xmlSource, String sonarPath){
+		try {		
+			String projectName = settings.getProperties().get("sonar.projectName");
+			TransformerFactory tFactory=TransformerFactory.newInstance();
+			Source xslDoc=new StreamSource("resources/Sonar-Report.xsl");
+			String outputFileName=sonarPath+File.separator+projectName+"_Report.html";
 
-	/*
-	 * public static void main(String[] args) {
-	 * //System.setProperty(Simplelo.DEFAULT_LOG_LEVEL_KEY, "TRACE");
-	 * SonarReport r = new SonarReport(); r.generate(); }
-	 */
+			OutputStream htmlFile=new FileOutputStream(outputFileName);
+			Transformer trasform=tFactory.newTransformer(xslDoc);
+			trasform.transform(xmlSource, new StreamResult(htmlFile));
+			log.info("Report generated in "+outputFileName);
+		} 
+		catch (Exception e) {
+			throw new SonarReporException(e);
+		}
+	}
 
 }
